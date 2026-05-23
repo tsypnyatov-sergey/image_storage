b10a67984e2530f7e3a827392cb47f32f5272881
tsypnyatov-sergey <tsypnyatov2012@gmail.com>
2026-05-23 18:42:56 +0300

    fix: nginx serve static files

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from app.settings import LOG_PATH
 
 
 logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s %(levelname)s %(message)s',
+                    format='[%(asctime)s] %(levelname)s: %(message)s',
                     datefmt='%m/%d/%Y %I:%M:%S %p',
                     handlers=[
                         logging.StreamHandler(),
